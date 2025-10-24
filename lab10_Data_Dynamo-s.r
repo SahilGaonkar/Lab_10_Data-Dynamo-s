651cf1f6c87f34f9c530a6bb9fabb26707e474cc
Sahil AG <sahilgaonkar67@gmail.com>
2025-10-24 23:47:36 +0530

    Structure

---
+++ lab10_Data_Dynamo-s.r
@@ -0,0 +1,37 @@
+# ====================================================================
+# lab10_Data-Dynamo-s.R
+# Collaborative R Visualization Project
+# Dataset: airquality 
+# Team Members:
+# Esha Kambli 2301
+# Deversh Shetgaonkar 2302
+# Sahil Gaonkar 2305
+# Prabhanjan Halvegar 2306
+# Atharv Gawas 2313
+# Aarchi Teli 2318 
+# Sarvadhnya Patil 2321
+# Harsh Palyekar 2329
+# ====================================================================
+
+# Load dataset
+data(airquality)
+
+# View structure
+str(airquality)
+
+
+#Pie Chart 
+
+# Bar Chart
+
+# Line Chart
+
+# Stacked Bar Chart
+
+# Histogram
+
+# Dot Chart
+
+# Scatter Chart
+
+# Box Plot
